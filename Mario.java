fe020eaeb61deedf913a02f1489e26b975da9b10
Dana Wanner <rennaw63@gmail.com>
2014-05-21 12:10:39 -0500

    this version allows the user to print both to the console as well as to a file.

--- Mario.java
+++ Mario.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 /**
@@ -6,40 +9,50 @@ import java.util.Scanner;
 public class Mario {
 
 
-        public static void main (String[] args) {
+
+
+        public static void main (String[] args) throws IOException{
 
             int height;
 
 
+
+
             while (true){
-                System.out.println("Give me a number between 0 and 23 \n");
+                System.out.println("Give me a number between 0 and 23: ");
                 Scanner in = new Scanner(System.in);
 
                 height = in.nextInt();
 
                 if ((height <= 1) || (height > 23))
-                    System.out.println("ERROR Please enter a valid age\n");
+                    System.out.println("ERROR Please enter a valid number\n");
                 else
                     break;
             }
 
 
 
-//          print spaces
+            File file = new File("mario.txt");
+            PrintWriter output = new PrintWriter(file);
+
             for (int i = 1; i <= height; i++) {
-                //print spaces
+//            print spaces
                 for (int s = height - i; s > 0; s--) {
                     System.out.print(" ");
+                    output.print(" ");
                 }
 
 //          print hashes
                 for (int j = 0; j <= i; j++) {
                     System.out.print("#");
+                    output.print("#");
 
                 }
                 //print new_line
                 System.out.print("\n");
+                output.print("\n");
             }
+            output.close();
         }
 
 }
